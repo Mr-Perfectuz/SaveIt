efbcd7e20784d387b1200de27e02350a2295ed9b
Anvar <ziyodovanvar1999@gmail.com>
2024-11-24 17:53:13 +0900

    Feat: develop auth layot

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignIn = () => {
+  return <div>SinIn</div>;
+};
+
+export default SignIn;
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SignUp = () => {
+  return <div>SinUp</div>;
+};
+
+export default SignUp;
