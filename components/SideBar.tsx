7eb4aa02028c9ed787acde51316f596855680973
Anvar <ziyodovanvar1999@gmail.com>
2024-12-01 17:49:50 +0900

    Feat: develop Header and SideBar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,7 +1,20 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const SideBar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <aside className='sidebar'>
+      <Link href='/'>
+        <Image
+          src='/assets/icons/logo.jpg'
+          alt='logo'
+          width={160}
+          height={50}
+        />
+      </Link>
+    </aside>
+  );
 };
 
 export default SideBar;
