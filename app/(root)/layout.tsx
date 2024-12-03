36d5ba4107be56c1134a4ce2cb3cfb3919fed254
Anvar <anvar.ziyodov@trescal.com>
2024-12-03 14:05:16 +0900

    Feat: develop MobileNavigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,20 +1,20 @@
-import Header from '@/components/Header';
-import MobileNavigation from '@/components/MobileNavigation';
-import SideBar from '@/components/SideBar';
-import { getCurrentUser } from '@/lib/actions/user.action';
-import { redirect } from 'next/navigation';
-import React from 'react';
+import Header from "@/components/Header";
+import MobileNavigation from "@/components/MobileNavigation";
+import SideBar from "@/components/SideBar";
+import { getCurrentUser } from "@/lib/actions/user.action";
+import { redirect } from "next/navigation";
+import React from "react";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
-  if (!currentUser) return redirect('/sign-in');
+  if (!currentUser) return redirect("/sign-in");
   return (
-    <main className='flex h-screen'>
+    <main className="flex h-screen">
       <SideBar {...currentUser} />
-      <section className='flex h-full flex-1 flex-col'>
-        <MobileNavigation />
+      <section className="flex h-full flex-1 flex-col">
+        <MobileNavigation {...currentUser} />
         <Header />
-        <div className='main-content'>{children}</div>
+        <div className="main-content">{children}</div>
       </section>
     </main>
   );
