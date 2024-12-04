3d818ffeb1c01beea9e8181d019e176ee62acf15
Anvar <anvar.ziyodov@trescal.com>
2024-12-04 17:11:08 +0900

    Feat: develop ActionsModalContent component

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -110,8 +110,6 @@ export const getFiles = async ({
       appwriteConfig.filesCollectionId,
       queries
     );
-
-    console.log({ files });
     return parseStringify(files);
   } catch (error) {
     handleError(error, "Failed to get files");
